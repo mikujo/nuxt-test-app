dce765452ebda65ad1d1c8d68e720437ec99afdd
mikujo <jo.tomorrow.0405@gmail.com>
2022-10-27 13:17:07 +0900

    画面遷移の設定

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
   <section class="container">
-    <p>Hello World</p>
-  </section></template>
+    <p>{{ message }}</p>
+    <hr />
+    <ul>
+      <li>
+        <router-link to="/price">Price Page</router-link>
+      </li>
+      <li>
+        <router-link to="/mypage">My Page</router-link>
+      </li>
+    </ul>
+  </section>
+</template>
+
 <script>
-export default {}
+export default {
+  data: function() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
 </script>
